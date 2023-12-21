c6ba8f402b5516819b7ba408a4ea9fbe529fc669
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-12-21 17:43:28 +0200

    feat: add open and close modal

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
+// const baseUrl = window.location.origin;
+const baseUrl = '../assets/images/'
 let products = [
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-1.jpg",
+    image: 'coffee-1.jpg',
     tittle: "Irish coffee",
     description:
       "Fragrant black coffee with Jameson Irish whiskey and whipped milk",
@@ -9,7 +11,7 @@ let products = [
   },
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-2.jpg",
+    image: 'coffee-2.jpg',
     tittle: "Kahlua coffee",
     description:
       "Classic coffee with milk and Kahlua liqueur under a cap of frothed milk",
@@ -17,28 +19,28 @@ let products = [
   },
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-3.jpg",
+    image: '../assets/images/coffee-3.jpg',
     tittle: "Honey raf",
     description: "Espresso with frothed milk, cream and aromatic honey",
     price: "$5.50",
   },
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-4.jpg",
+    image: '../assets/images/coffee-4.jpg',
     tittle: "Ice cappuccino",
     description: "Cappuccino with soft thick foam in summer version with ice",
     price: "$5.00",
   },
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-5.jpg",
+    image: `${baseUrl}/assets/images/coffee-5.jpg`,
     tittle: "Espresso",
     description: "Classic black coffee",
     price: "$4.50",
   },
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-6.jpg",
+    image: `${baseUrl}/assets/images/coffee-6.jpg`,
     tittle: "Latte",
     description:
       "Espresso coffee with the addition of steamed milk and dense milk foam",
@@ -46,21 +48,21 @@ let products = [
   },
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-7.jpg",
+    image: `${baseUrl}/assets/images/coffee-7.jpg`,
     tittle: "Latte macchiato",
     description: "Espresso with frothed milk and chocolate",
     price: "$5.50",
   },
   {
     categories: "coffee",
-    image: "../assets/images/coffee/coffee-8.jpg",
+    image: `${baseUrl}/assets/images/coffee-8.jpg`,
     tittle: "Coffee with cognac",
     description: "Fragrant black coffee with cognac and whipped cream",
     price: "$6.50",
   },
   {
     categories: "tea",
-    image: "../assets/images/tea/tea-1.jpg",
+    image: `${baseUrl}/assets/images/tea-1.jpg`,
     tittle: "Moroccan",
     description:
       "Fragrant black tea with the addition of tangerine, cinnamon, honey, lemon and mint",
@@ -68,21 +70,21 @@ let products = [
   },
   {
     categories: "tea",
-    image: "../assets/images/tea/tea-2.jpg",
+    image: `${baseUrl}/assets/images/tea-2.jpg`,
     tittle: "Ginger",
     description: "Original black tea with fresh ginger, lemon and honey",
     price: "$5.00",
   },
   {
     categories: "tea",
-    image: "../assets/images/tea/tea-3.jpg",
+    image: `${baseUrl}/assets/images/tea-3.jpg`,
     tittle: "Cranberry",
     description: "Invigorating black tea with cranberry and honey",
     price: "$5.00",
   },
   {
     categories: "tea",
-    image: "../assets/images/tea/tea-4.jpg",
+    image: `${baseUrl}/assets/images/tea-4.jpg`,
     tittle: "Sea buckthorn",
     description:
       "Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon",
@@ -90,7 +92,7 @@ let products = [
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-1.jpg",
+    image: `${baseUrl}/assets/images/dessert-1.jpg`,
     tittle: "Marble cheesecake",
     description:
       "Philadelphia cheese with lemon zest on a light sponge cake and red currant jam",
@@ -98,14 +100,14 @@ let products = [
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-2.jpg",
+    image: `${baseUrl}/assets/images/dessert-2.jpg`,
     tittle: "Red velvet",
     description: "Layer cake with cream cheese frosting",
     price: "$4.00",
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-3.jpg",
+    image: `${baseUrl}/assets/images/dessert-3.jpg`,
     tittle: "Cheesecakes",
     description:
       "Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar",
@@ -113,7 +115,7 @@ let products = [
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-4.jpg",
+    image: `${baseUrl}/assets/images/dessert-4.jpg`,
     tittle: "Creme brulee",
     description:
       "Delicate creamy dessert in a caramel basket with wild berries",
@@ -121,28 +123,28 @@ let products = [
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-5.jpg",
+    image: `${baseUrl}/assets/images/dessert-5.jpg`,
     tittle: "Pancakes",
     description: "Tender pancakes with strawberry jam and fresh strawberries",
     price: "$4.50",
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-6.jpg",
+    image: `${baseUrl}/assets/images/dessert-6.jpg`,
     tittle: "Honey cake",
     description: "Classic honey cake with delicate custard",
     price: "$4.50",
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-7.jpg",
+    image: `${baseUrl}/assets/images/dessert-7.jpg`,
     tittle: "Chocolate cake",
     description: "Cake with hot chocolate filling and nuts with dried apricots",
     price: "$5.50",
   },
   {
     categories: "dessert",
-    image: "../assets/images/dessert/dessert-8.jpg",
+    image: `${baseUrl}/assets/images/dessert-8.jpg`,
     tittle: "Black forest",
     description:
       "A combination of thin sponge cake with cherry jam and light chocolate mousse",
@@ -152,6 +154,7 @@ let products = [
 
 const productList = document.querySelector("[data-product-list]");
 const buttonsCategory = document.querySelectorAll("[data-button-category]");
+//create current button of category
 function activeButton(value){
     let current = '';
     buttonsCategory.forEach((button) => {
@@ -164,10 +167,12 @@ function activeButton(value){
           });
     return current;
 }
+// create marcup product
 function createMarcup(cardList){
     const markup = cardList.map(({ image, tittle, description, price }) => `
-    <li class="menu-card__item" data-modal-open>
-                    <div class="menu-card__image"><img  src=${image} alt=${tittle}></div>
+    <li class="menu-card__item">
+    <button class="menu-card__button"  onClick="openModal()" type="button">
+                    <div class="menu-card__image"><img  src="${image}" alt=${tittle}></div>
                     <div class="menu-card__description">
                         <div class="menu-card__top">
                             <p class="menu-card__title">${tittle}</p>
@@ -175,11 +180,13 @@ function createMarcup(cardList){
                         </div>
                         <p class="menu-card__price">${price}</p>
                     </div>
+      </button>            
     </li>
 `)
 .join("");
 productList.innerHTML = markup;
 }
+// create marcup all product of one category
 function filterProduct(value){
     const currentCategory = activeButton(value)
     const cardList = products.filter(cardsCurrent => cardsCurrent.categories.toUpperCase() === currentCategory)
@@ -189,3 +196,19 @@ function filterProduct(value){
 window.onload = () => {
    filterProduct("coffee");
 };
+
+const modal = document.querySelector("[data-modal]");
+const productButtons = document.querySelectorAll(".menu-card__button");
+productButtons.forEach(button => {button.addEventListener("click", openModal)});
+modal.addEventListener("click", closeModal);
+
+function openModal(cardData) {
+  modal.classList.remove("is-hidden");
+  document.body.classList.add("lock");
+}
+function closeModal(){
+  modal.classList.add("is-hidden");
+  document.body.classList.remove("lock");
+}
+
+
