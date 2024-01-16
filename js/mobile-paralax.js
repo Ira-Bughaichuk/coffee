3ff16c62d4802d4a0812bb7b46007c131bc9554f
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-16 15:46:58 +0200

    fix project

--- js/mobile-paralax.js
+++ js/mobile-paralax.js
@@ -6,8 +6,8 @@ function parallax(e) {
   this.querySelectorAll('.layer').forEach(layer => {
     const speed = layer.getAttribute('data-speed');
 
-    const x = (window.innerWidth - e.pageX * speed) / 100;
-    const y = (window.innerHeight - e.pageY * speed) / 100;
+    const x = (window.innerWidth - e.pageX * speed) / 300;
+    const y = (window.innerHeight - e.pageY * speed) / 300;
 
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
